cfcfe9ccb9a348f05921a5eb205e1a152e3bc5e0
Moiseev Aleksej <moiseev.003@bk.ru>
2022-03-27 09:55:09 +0300

    Add tables

--- adapters/database/tables.py
+++ adapters/database/tables.py
@@ -1,8 +1,34 @@
 from sqlalchemy import (
-    MetaData, Table, Column, Integer, String, Float, ForeignKey
+    MetaData, Table, Column, Integer, String, Float, ForeignKey, Date, Text
 )
 from sqlalchemy.orm import registry, relationship
 
 
 mapper_registry = registry()
 metadata = MetaData()
+
+users = Table(
+    'users', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('username', String(30)),
+    Column('email', String(30)),
+    Column('password', String(30)),
+    Column('access_token', String(128)),
+    Column('refresh_token', String(128)),
+)
+
+chats = Table(
+    'chats', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('owner',  ForeignKey('users.id')),
+    Column('descriptions', Text),
+    Column('created', Integer, Date),
+)
+
+messages = Table(
+    'messages', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('user_id',  ForeignKey('users.id')),
+    Column('chat_id', ForeignKey('users.id')),
+    Column('created', Integer, Date),
+)
