603e57fc849b384a254ea9c5cb59724e55111bdc
Moiseev Aleksej <moiseev.003@bk.ru>
2022-04-06 15:07:27 +0300

    add testing fot userservice

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
-import datetime
 from unittest.mock import Mock
 
 import pytest
 
-from application import interfaces
-from application.dto import User, Chat, Message, ChatMember
-from application.services import UserService, MessageService, ChatService, ChatMemberService
+from application.user_application import interfaces
+from application.user_application.dataclases import User
+from application.user_application.services import UserService
 
 ACCESS_TOKEN = " Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwayI6MH0.99Q7zHnAZVqblwg93gjXYRCB-LmOuhfWjWWqx0ei5fg"
 REFRESH_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9" \
@@ -45,28 +44,19 @@ def user(params):
 
 
 @pytest.fixture
-def user1():
-    return User(
-        pk=0,
-        username='Alex',
-        email='alex@mail.ru',
-        password='password',
-        access_token=ACCESS_TOKEN,
-    )
+def user1(user):
+    user.id = 0
+    return user
 
 
 @pytest.fixture
 def users(params):
     actors = []
     for i in range(10):
-        actors.append(User(pk=i, **params))
+        user = User(**params)
+        user.id = i
+        actors.append(user)
     return actors
-
-
-@pytest.fixture
-def owner(user):
-    user.pk = 0
-    return user
 
 
 @pytest.fixture(scope='function')
@@ -78,139 +68,13 @@ def user_repo(user, user1, users):
     return user_repo
 
 
+@pytest.fixture(scope='function')
+def publisher():
+    return "message"
+
+
 @pytest.fixture
 def user_service(user_repo):
-    return UserService(repository=user_repo)
+    return UserService(repository=user_repo, publisher=publisher)
 
 
-@pytest.fixture
-def message():
-    params = {
-        'user_id': 0,
-        'chat_id': 0,
-        'body': "Hello"
-    }
-    return Message(**params)
-
-
-@pytest.fixture
-def message0():
-    params = {
-        'pk': 0,
-        'user_id': 0,
-        'chat_id': 0,
-        'body': "Hello"
-    }
-    return Message(**params)
-
-
-@pytest.fixture
-def messages():
-    params = {
-        'user_id': 0,
-        'chat_id': 0,
-        'body': "Hello"
-    }
-    ms = []
-    for i in range(10):
-        ms.append(Message(pk=i, **params))
-    return ms
-
-
-@pytest.fixture(scope='function')
-def message_repo(message0, messages):
-    message_repo = Mock(interfaces.MessageRepositoryInterface)
-    message_repo.add = Mock(return_value=message0)
-    message_repo.get = Mock(return_value=message0)
-    message_repo.get_list = Mock(return_value=messages)
-    return message_repo
-
-
-@pytest.fixture
-def message_service(message_repo):
-    return MessageService(messages_repo=message_repo)
-
-
-@pytest.fixture
-def params_chat(owner):
-    return {
-        'title': "title",
-        'descriptions': 'descriptions',
-        'owner': owner.pk
-    }
-
-
-@pytest.fixture
-def chat(params_chat):
-    return Chat(**params_chat)
-
-
-@pytest.fixture
-def chat0(params_chat):
-    return Chat(pk=0, **params_chat)
-
-
-@pytest.fixture
-def chats(params_chat):
-    chs = []
-    for i in range(10):
-        chs.append(Chat(pk=i, **params_chat))
-    return chs
-
-
-@pytest.fixture(scope='function')
-def chat_repo(chat0, chats):
-    chat_repo = Mock(interfaces.ChatRepositoryInterface)
-    chat_repo.add = Mock(return_value=chat0)
-    chat_repo.get = Mock(return_value=chat0)
-    chat_repo.get_list = Mock(return_value=chats)
-    chat_repo.delete = Mock(return_value=chat0)
-    return chat_repo
-
-
-@pytest.fixture
-def chat_member_params():
-    return {
-        'user_id': 0,
-        'chat_id': 0,
-    }
-
-
-@pytest.fixture
-def chat_member(chat_member_params):
-    return ChatMember(**chat_member_params)
-
-
-@pytest.fixture
-def chat_member0(chat_member_params):
-    return ChatMember(pk=0, **chat_member_params)
-
-
-@pytest.fixture
-def chat_members(chat_member_params):
-    cms = []
-    for i in range(10):
-        cms.append(ChatMember(pk=i, **chat_member_params))
-    cms[0].kicked = datetime.datetime.now()
-    cms[-1].kicked = datetime.datetime.now()
-    return cms
-
-
-@pytest.fixture(scope='function')
-def chat_member_repo(chat_member0, chat_members):
-    chat_member_repo = Mock(interfaces.ChatMembersRepositoryInterface)
-    chat_member_repo.add = Mock(return_value=chat_member0)
-    chat_member_repo.get = Mock(return_value=chat_member0)
-    chat_member_repo.get_list = Mock(return_value=chat_members)
-    chat_member_repo.delete = Mock(return_value=chat_member0)
-    return chat_member_repo
-
-
-@pytest.fixture(scope='function')
-def chat_member_service(chat_member_repo):
-    return ChatMemberService(chat_member_repo=chat_member_repo)
-
-
-@pytest.fixture(scope='function')
-def chat_service(chat_repo, chat_member_repo):
-    return ChatService(chats_repo=chat_repo, members_repo=chat_member_repo)
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,21 +1,17 @@
 import pytest
 
 
-Base_URL = 'http://127.0.0.1:8080/'
-
-
 class TestUserService:
 
     def test_register_user(self, user, user_service):
         actor = user_service.register(user)
-        assert actor.pk == 0
+        assert actor.id == 0
 
     def test_get_user(self, user1, user_service):
-        actor = user_service.get_user(pk=user1.pk)
+        actor = user_service.get_user(pk=user1.id)
         assert actor.username == user1.username
         assert actor.email == user1.email
         assert actor.password == user1.password
-        assert actor.access_token == user1.access_token
 
     def test_get_users(self, users, user_service):
         actors = user_service.get_users()
@@ -25,77 +21,3 @@ class TestUserService:
     def test_get_users_by_limit(self, limit, offset, user_service, user_repo):
         user_service.get_users(limit=limit, offset=offset)
         user_repo.get_list.assert_called_with(limit=limit, offset=offset)
-
-
-class TestChatService:
-
-    def test_register_chat(self, chat_service, chat):
-        ch = chat_service.register_chat(chat)
-        assert ch.pk == 0
-
-    def test_create_chat(self, chat_service, chat, chat0):
-        assert chat_service.create_chat(chat) == chat0
-
-    def test_delete_chat(self, chat0, chat_service):
-        assert chat_service.delete_chat(chat0.pk) == chat0
-
-    def test_get_chat(self, chat0, chat_service):
-        assert chat_service.get_chat(chat0.pk) == chat0
-
-    def test_get_chats(self, chats, chat_service):
-        ch = chat_service.get_chats()
-        assert len(ch) == len(chats)
-
-
-class TestMessageService:
-
-    def test_send_message(self, message_service, message, message0):
-        m = message_service.send_message(message)
-        assert m.pk == 0
-
-    def test_get_messages(self, message_service, messages):
-        ms = message_service.get_messages()
-        assert len(ms) == len(messages)
-
-    @pytest.mark.parametrize('chat_id, expected', [(0, 10), (1, 0)])
-    def test_get_messages_by_chat_id(self, chat_id, expected, message_service):
-        assert len(message_service.get_messages_by_chat_id(
-            chat_id=chat_id,
-            user_id=None,
-            limit=None,
-            offset=None,)) == expected
-
-    @pytest.mark.parametrize('limit, offset', [(1, 3), (3, 1)])
-    def test_get_messages_by_limit(self, limit, offset, message_service, message_repo):
-        message_service.get_messages(limit=limit, offset=offset)
-        message_repo.get_list.assert_called_with(limit=limit, offset=offset)
-
-
-class TestChatMemberService:
-
-    def test_get_members(self, chat_member_service, chat_members):
-        cms = chat_member_service.get_members()
-        assert len(cms) == len(chat_members)
-
-    def test_create_members(self, chat_member_service, chat_member):
-        cm = chat_member_service.create_members(chat_member)
-        assert cm.pk == 0
-
-    @pytest.mark.parametrize('chat_id, expected', [(0, 8), (1, 0)])
-    def test_get_members_by_chat(self, chat_id, expected, chat_member_service):
-        assert len(chat_member_service.get_members_by_chat(chat_id=chat_id)) == expected
-
-    def test_get_member(self, chat_member_service, chat_member0):
-        assert chat_member_service.get_member(chat_member0.pk) == chat_member0
-
-    def test_is_owner(self, owner, chat, chat_member_service):
-        assert chat_member_service.is_owner(owner, chat) is True
-
-    def test_is_member(self, user1, chat0, chat_member_service):
-        assert chat_member_service.is_owner(user1, chat0) is True
-
-    def test_add_member_to_chat(self, chat_member_service, chat_member, chat_member0):
-        assert chat_member_service.add_member_to_chat(chat_member) == chat_member0
-
-    def test_del(self, chat_member_service, user1, chat0, chat_member0):
-        assert chat_member_service.delete_member(user1.pk, chat0.pk) == chat_member0
